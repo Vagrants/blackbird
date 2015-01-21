a654280f2616665fd157517e3df1e203195c9dfe
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2015-01-21 22:35:19 +0900

    Implement prototype get_handler_fp
    * This function will be integrated LoggerFaactory Abstract Object in the future

--- blackbird/utils/logger.py
+++ blackbird/utils/logger.py
@@ -5,6 +5,8 @@ import logging.handlers
 import sys
 import platform
 
+import blackbird.utils.error
+
 
 def logger_factory(filename, level, fmt='ltsv'):
 
@@ -58,3 +60,27 @@ def logger_factory(filename, level, fmt='ltsv'):
     logger.addHandler(handler)
 
     return logger
+
+
+def get_handler_fp(logger):
+    """
+    Get handler_fp. This method is integrated to LoggerFactory Object in the future.
+    :param logging.Logger logger: Python logging.Logger. logger instance.
+    :rtype: logging.Logger.handlers.BaseRotatingHandler
+    :return: Handler or Handler's stream. We call it `handler_fp`.
+    """
+    if not hasattr(logger, 'handlers'):
+        raise blackbird.utils.error.BlackbirdError(
+            'Given logger is not logging.Logger instance!'
+        )
+
+    if len(logger.handlers) != 1:
+        raise blackbird.utils.error.BlackbirdError(
+            'Given logger has invalid handlers.'
+        )
+
+    if hasattr(logger.handlers[0], 'stream'):
+        return logger.handlers[0].stream
+
+    # case of setting SysLogHandler to logger.handlers[0]
+    return logger.handlers[0]

0868f6eb3d7283817a46c7fd5473c1b2651a2228
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2015-04-25 11:27:54 +0900

    Upgrade configobj 5.x in only setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     },
     install_requires=[
         "argparse",
-        "configobj>=4.0,<5.0",
+        "configobj>=5.0",
         "lockfile",
         "logilab-astng",
         "logilab-common",

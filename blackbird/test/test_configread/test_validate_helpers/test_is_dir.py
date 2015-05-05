418497eb2a364b7195475142a9c1d2bb49739310
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2015-05-05 12:58:46 +0900

    Separate following two type unit tests
    1. It need to create new directories.
    2. It doesn't need to create any.

--- blackbird/test/test_configread/test_validate_helpers/test_is_dir.py
+++ blackbird/test/test_configread/test_validate_helpers/test_is_dir.py
@@ -27,10 +27,23 @@ class TestConfigReaderIsDir(object):
     def test_exist_file(self):
         blackbird.utils.configread.is_dir(__file__)
 
-    @nose.tools.raises(validate.VdtValueError)
-    def test_non_read_permission(self):
-        value = os.path.join(
+
+class TestConfigReaderIsDirCaseOfCreatingDirectory(object):
+
+    def __init__(self):
+        self.cannot_read_dir = os.path.join(
             os.path.dirname(__file__),
             'cannot_read_dir'
         )
-        blackbird.utils.configread.is_dir(value)
+
+    def setup(self):
+        os.mkdir(self.cannot_read_dir, 0o000)
+
+    def teardown(self):
+        os.rmdir(self.cannot_read_dir)
+
+    @nose.tools.raises(validate.VdtValueError)
+    def test_non_read_permission(self):
+        blackbird.utils.configread.is_dir(self.cannot_read_dir)
+
+
